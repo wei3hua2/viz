279ef691b0c5fd9015f351e4b226ec9e869b2f47
James <email@jameschong.me>
2025-01-14 11:07:33 +0800

    bounty

---
+++ scripts/bounty.js
@@ -0,0 +1,13 @@
+
+
+
+export async function init () {
+
+    const data = (await axios.get("./data/papers/bounty.json")).data;
+
+    let table = new DataTable('#papers_table', {
+        pageLength:1000,
+        data,
+        columns:[ {data:'title'}, {data:'year'}, {data:'topics'} ]
+    });
+}
