8f94864461cf1a9f53da911a4b90b7ef3415211b
James <email@jameschong.me>
2025-02-21 10:50:19 +0800

    update

---
+++ scripts/recon.js
@@ -0,0 +1,11 @@
+
+export async  function init () {
+    const books = (await axios.get("./data/cybersec/books.json")).data;
+
+    let table = new DataTable('#books_table', {
+        pageLength:1000,
+        data:books,
+        columns:[ {data:'title'}, {data:'year'}, {data:'topics'} ]
+    });
+      
+};
