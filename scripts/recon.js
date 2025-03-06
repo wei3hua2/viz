ea383691e0b78c73fa0d1edc80afda3cb964d2d0
James <email@jameschong.me>
2025-03-06 19:22:47 +0800

    recon tools

--- scripts/recon.js
+++ scripts/recon.js
@@ -1,11 +1,46 @@
 
-export async  function init () {
-    const books = (await axios.get("./data/cybersec/books.json")).data;
 
-    let table = new DataTable('#books_table', {
+
+
+const tools = [
+    {"name":"dig",          "detail":"", "tags":["manual","dns"]},
+    {"name":"whois",        "detail":"whoisRWS, whoisology, DomainTools", "tags":["manual","dns"]},
+    {"name":"dorks",        "detail":"Inurlbr", "tags":["manual","dns"]},
+    {"name":"dnsdumpster",  "detail":"", "tags":["manual","dns"]},
+    {"name":"hackertarget", "detail":"", "tags":["manual","dns"]},
+    {"name":"shodan",       "detail":"", "tags":["manual","dns"]},
+    {"name":"censys",       "detail":"", "tags":["manual","dns","ct"]},
+    {"name":"fierce",       "detail":"", "tags":["manual","dns"]},
+    {"name":"sublist3r",    "detail":"", "tags":["manual","dns"]},
+    {"name":"enumall",      "detail":"", "tags":["manual","dns"]},
+
+    {"name":"spiderFoot",   "detail":"", "tags":['automated']},
+    {"name":"intrigue.io",  "detail":"", "tags":['automated']},
+    {"name":"Recon-NG",     "detail":"", "tags":['automated']},
+
+    {"name":"BuiltWith",    "detail":"", "tags":['website']},
+    {"name":"WIG",          "detail":"webapp information gatherer", "tags":['website']},
+    {"name":"CMSmap",       "detail":"", "tags":['website']},
+    {"name":"WPScan",       "detail":"", "tags":['website']},
+
+    {"name":"Dirhunt",       "detail":"", "tags":['directory']},
+    {"name":"Wfuzz",         "detail":"", "tags":['directory']},
+    {"name":"Photon",        "detail":"", "tags":['directory']},
+
+    {"name":"CTFR",          "detail":"", "tags":['ct']},
+    {"name":"crt.sh",        "detail":"", "tags":['ct']},
+    {"name":"CloudFlair",    "detail":"", "tags":['ct']},
+    {"name":"CachedView.com","detail":"", "tags":['ct']},
+    {"name":"Wayback Machine Scraper",    "detail":"", "tags":['ct']},
+]
+
+
+export async  function init () {
+
+    let table = new DataTable('#network_table', {
         pageLength:1000,
-        data:books,
-        columns:[ {data:'title'}, {data:'year'}, {data:'topics'} ]
+        data:tools,
+        columns:[ {data:'name'}, {data:'detail'}, {data:'tags'} ]
     });
       
 };
